96711bb84656e71f53bed4132d9d27fd94337927
216037150 <siyabongazungu95@gmail.com>
2024-11-03 18:01:41 -0500

    req, res

--- server/server.js
+++ server/server.js
@@ -6,11 +6,11 @@ app.get('/', (req, res) => {
     res.send("<h1>Welcome to my home page</h1>")
 });
 
-app.get('/about',(res, req)=>{
+app.get("/about",(req, res)=>{
     res.send("<h1>My about page</h1>")
 });
 
-app.use((res, req)=>{
+app.use((req, res)=>{
     res.status(400).send("Page not Found")
 });
 
